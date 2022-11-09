b2fcb2b1b75352aae7380f9fdcc9e4d65b57ce37
arturaleszczyk <artur.aleszczyk@icloud.com>
2022-11-09 13:42:21 +0100

    duchy mogą poruszać się tylko do przodu

--- Gra/ghost.py
+++ Gra/ghost.py
@@ -10,16 +10,17 @@ def ghost_should_move_up():
     ghost_should_move +=1
 class Ghost:
     def __init__(self): # pozycja każdego ducha (4 duchy )
-        self.ghosts = [Actor(f'ghost{i}', pos=(250+(i*20), 370)) for i in range(1, 5)] # dla każdego ducha tworzymy aktora, ustawiamy pozycję, każdy gdzie indziej to x natomiast Y jest stałe
+        self.ghosts = [Actor(f'ghost{i}', pos=(250+(i*20), 370), anchor=(14, 8)) for i in range(1, 5)] # dla każdego ducha tworzymy aktora, ustawiamy pozycję, każdy gdzie indziej to x natomiast Y jest stałe
         for ghost in self.ghosts:
             ghost.dir = randint(0, 3)
+            ghost.last_dir = -100
             ghost.status = 0
             ghost.in_center = True
-            ghost.decide_point = 1, 1
+            ghost.decide_point = 0, 8
         self.disable_ghost_image = 'ghost5'   # piąty duch kiedy zostanie zabrany bonus
         self.enable = True # wszystkie duchy włączone
         self.disable_time = 0 # po zabraniu bonusu duchy są wyłączone
-        self.ghost_moves = (1, 0), (0, -1), (-1, 0), (0, 1)# jakie mają kierunku prawo góra lewo dół (x,y) (1,0) x - rusza się w prawo lewo y - góra dół
+        self.ghost_moves = (18, 0), (0, -18), (-18, 0), (0, 18)# jakie mają kierunku prawo góra lewo dół (x,y) (1,0) x - rusza się w prawo lewo y - góra dół
         self.ghost_speed = 3  # prędkość duchów
 
     def diable_ghost(self):
@@ -41,8 +42,18 @@ class Ghost:
         for ghost in self.ghosts:
             ghost.draw()
 
+    def ghost_in_center(self):
+        for ghost in self.ghosts:
+            if 231 < ghost.x < 370 and 257 + 80 < ghost.y < 337+80:
+                ghost.in_center = True
+            else:
+                ghost.in_center = False
+
     def update(self):
+        self.ghost_in_center()
         self.move_ghost()
+        for ghost in self.ghosts:
+            ghost.last_dir = ghost.dir
 
     def move_ghost(self):
         global ghost_should_move
@@ -52,10 +63,10 @@ class Ghost:
         for ghost in self.ghosts:
             directions = get_possible_directions(ghost)
             ghost.dir = randint(0, 3) # losowanie w którą stronę idzie
-            while directions[ghost.dir] == 0: # jeżeli tafimy na brak ruchu czyli 0 losujemy ponownie
+            while directions[ghost.dir] == 0 or (abs(ghost.last_dir - ghost.dir) == 2 and directions.count(1) > 1): # jeżeli tafimy na brak ruchu czyli 0 losujemy ponownie
                 ghost.dir = randint(0, 3)
-            animate(ghost, pos=(ghost.x + self.ghost_moves[ghost.dir][0]*20,
-                                ghost.y + self.ghost_moves[ghost.dir][1]*20),
+            animate(ghost, pos=(ghost.x + self.ghost_moves[ghost.dir][0],
+                                ghost.y + self.ghost_moves[ghost.dir][1]),
                     duration=1/self.ghost_speed, tween='linear', on_finished=ghost_should_move_up) # wybieramy współrzędną X i razy 20
 
 
