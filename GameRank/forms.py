8fd7a7cf289dbf4b43cad6e856ec9d7fae129813
marcindarecki <marcin.darecki@gmail.com>
2022-11-14 20:07:22 +0100

    Adding admin

--- GameRank/forms.py
+++ GameRank/forms.py
@@ -6,7 +6,7 @@ from django.forms import (
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 
-from GameRank.models import Genre, Movie
+from GameRank.models import Genre, GameRank1
 from datetime import date
 
 
@@ -26,14 +26,14 @@ class PastMonthField(DateField):
         return date(year=result.year, month=result.month, day=1)
 
 
-class MovieForm(ModelForm):
+class GameRankForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
 
     class Meta:
-        model = Movie
+        model = GameRank1
         fields = '__all__'
     title = CharField(validators=[capitalized_validator])
     rating = IntegerField(min_value=1, max_value=10)
