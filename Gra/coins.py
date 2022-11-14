840fef951980fceb5d7d60e3a4f193ffc0d9ba76
arturaleszczyk <artur.aleszczyk@icloud.com>
2022-11-14 20:39:37 +0100

    zjadanie monet plus wizualizacja punktacji

--- Gra/coins.py
+++ Gra/coins.py
@@ -9,8 +9,8 @@ class Coin:
         self.power_name = 'power_up'
         self.coins = []
         # przechodzimy po mapie 600 x 600 co jeden pix
-        for x in range(0, 600, 1):
-            for y in range(0, 600, 1):
+        for x in range(20, 580, 1):
+            for y in range(20, 580, 1):
                 # sprawdzamy x i y
                 where_check = x, y
                 # sprawdzamy rodzaj kropki
@@ -26,9 +26,12 @@ class Coin:
                 if dot_type == 2:
                     new_coin = Actor(self.power_name, where_put, anchor=(13, 13) )
                     new_coin.type = 2
+                new_coin.hidden = False
                 self.coins.append(new_coin)
+        self.left_coins = len(self.coins)
     def draw_coins(self):
-        for coin in self.coins:
+        # rysujemy tylko odkryte
+        for coin in self.coins_to_draw():
             coin.draw()
 
     # jakie jeszcze będą monety
@@ -36,6 +39,29 @@ class Coin:
     def coins_to_draw(self):
         coins = []
         for coin in self.coins:
-            coins.append(coin)
+            if not coin.hidden:
+                coins.append(coin)
         return coins
 
+
+        # wpisujemy informacje o pacmanie
+
+    def check_collision(self, pacman):
+        x, y, width, height = pacman.x, pacman.y, pacman.width, pacman.height
+        # budujemy prostokąt obejmujący pacmana
+        # x to jest endpoint pacmana / 2 ustawiamy na połowę długości
+        rect = Rect(x - width / 2, y - height / 2, width, height)
+        # sprawdzamy czy pacman ma kolizję z monetą
+        for coin in self.coins_to_draw():
+            # jeżeli pacman ma kolizję z monetą, ukrywamy monetę
+            if coin.colliderect(rect):
+                coin.hidden = True
+                # jednocześnie zmniejszamy ilość monet
+                self.left_coins -= 1
+                if self.left_coins == 0:
+                    return "won"
+                if coin.type == 1:
+                    return "coin"
+                else:
+                    return "powerup"
+        return "None"
--- Gra/main.py
+++ Gra/main.py
@@ -8,10 +8,12 @@ from coins import Coin
 
 
 BLACK = (0,0,0)
+GOLD = 255, 215, 0
 WIDTH = 600
 HEIGHT = 680
 keys: keyboard
 screen: Screen
+POINTS = 0
 
 pacman = Pacman(keys)
 ghost = Ghost()
@@ -25,6 +27,9 @@ def draw():
     pacman.draw(screen)
     ghost.draw()
     coin.draw_coins()
+    # rysujemy ile mamy punktów
+    screen.draw.text(f'{POINTS}', color=GOLD, fontsize=50, fontname='bungee-regular',
+                     topright=(WIDTH - 10, 10), owidth=1, ocolor=(100, 100, 100))
 
 
 def on_key_down(key):
@@ -34,10 +39,26 @@ def on_key_down(key):
 def on_key_up(key):
     pacman.on_key_up(key)
 
+def update_by_coin():
+    global POINTS
+    # sprawdzamy czy jest wciśnięty klawisz pacmana
+    if pacman.move_pressed():
+        # jeżeli jest, sprawdzamy czy jest kolizja
+        coin_type = coin.check_collision(pacman.pacman)
+        # 3 rodzaje kolizji
+        if coin_type == "coin":
+            # o ile zwięlsza się nam punktacja
+            POINTS += 10
+        if coin_type == "powerup":
+            pass
+        if coin_type == "won":
+            pass
+
 
 def update():
     pacman.update()
     ghost.update(pacman.pacman.pos) # aktor klasy Pacman
+    update_by_coin()
 
 
 pgzrun.go()
