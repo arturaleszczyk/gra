189fe7b75b11914ee4944afe895d1094dcd7fa51
marcindarecki <marcin.darecki@gmail.com>
2022-11-14 18:48:28 +0100

    Adding admin

--- GameRank/apps.py
+++ GameRank/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class GamerankConfig(AppConfig):
+class GameRankConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'GameRank'
