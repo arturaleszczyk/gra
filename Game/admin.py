8fa0ae09523e76354be8cd11e8970c96a9b59f13
arturaleszczyk <artur.aleszczyk@icloud.com>
2022-11-29 12:04:37 +0100

    dodanie filtr oraz sreach

--- Game/admin.py
+++ Game/admin.py
@@ -7,6 +7,8 @@ from .models import GRA
 @admin.register(GRA)
 class AdminGra(admin.ModelAdmin):
     list_display = ['gracz','wynik']
+    list_filter = ['gracz']
+    search_fields = ['gracz', 'wynik']
 
 
 # Register your models here.
